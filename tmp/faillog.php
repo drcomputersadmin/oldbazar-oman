453bdddea1b8a66bb21845817a16debdc768e5eb
Rishana <info@drcomputers.ae>
2025-02-14 16:09:20 +0400

    initial commit

---
+++ tmp/faillog.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Login attempts info.
+*/
+$login_faillog = array (
+  0 => 
+  array (
+    '::1' => 0,
+    'last' => '',
+  ),
+  1 => 
+  array (
+    '94.59.13.227' => 0,
+    'last' => '',
+  ),
+);
